5e6fe7bef75bc20cba08dc4702722bdf4a13c303
Ishraq Bhuiyan <ishraqbhuiyan@gmail.com>
2016-02-11 10:15:27 -0500

    Potato

---
+++ canvas.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("ftb2maga");
+var ctx = canvas.getContext('2d');
+
+function makeDots(event){
+}
+
+//Sets the stroke to black
+ctx.strokeStyle = "#000000";
+//Draws a rectangle with upper left hand corner at 0,0 with height and width 538 and fill of Green
+ctx.strokeRect(0,0, 538, 538);
+
