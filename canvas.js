67a5cabd0f34b81679ccf36d40b9a4e4c837f25d
Ishraq Bhuiyan <ishraqbhuiyan@gmail.com>
2016-02-13 10:55:52 -0500

    something

--- canvas.js
+++ canvas.js
@@ -8,7 +8,15 @@ ctx.strokeRect(0,0, 538, 538);
 ctx.fillStyle = "#FF0000";
 
 var makeDots = function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  var pane = document.getElementById("ftb2maga").getConext('2d');
+  pane.arc(40,40,20,0,Math.Pi *2, false);
+  pane.beginPath();
+  pane.lineTo(x, y);
+  pane.arc(x,y,20,0, Math.Pi * 2);
+  path.moveTo(x,y);
+  pane.closePath();
 }
 
-ctx.addEventListener("mousedown", makeDots, false);
-
+canvas.addEventListener("mouseclick", makeDots, false);
